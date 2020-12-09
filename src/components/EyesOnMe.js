04eabc9555fe61e162c783eaf7e19c6bbd061f40
techtwins <gshevaughn@gmail.com>
2020-12-09 17:58:09 -0500

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+class EyesOnMe extends React.Component {
+  
+  focusBtn = () => console.log('Good!')
+
+  blurBtn = () => console.log('Hey! Eyes on me!')
+
+  render() {
+    return ( <button onFocus={this.focusBtn} onBlur={this.blurBtn}>Look Here</button> )
+  }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+import React from 'react'
+
+class Keypad extends React.Component {
+
+  passInput = () => console.log('Entering password...')
+
+  render() {
+    return ( <input onKeyUp={this.passInput} type="password"></input> )
+  }
+}
+
+export default Keypad
